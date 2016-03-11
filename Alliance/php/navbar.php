19040d6ad3e5c35d6cfe3227379b55949060dd73
Connor Dowson <connormwdowson@gmail.com>
2016-03-11 18:36:00 +0000

    added tsd coming soon site

---
+++ Alliance/php/navbar.php
@@ -0,0 +1,22 @@
+<head>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+
+</head>
+
+<nav class="flex">
+
+    <h1> <img src="assets/images/logo.png"> </h1>
+
+    <div class="links">
+    <ul>
+      <li> <a href='#'> Home </a> </li>
+      <li> <a href='#'> Roster </a> </li>
+      <li> <a href='#'> Achievements </a> </li>
+    </ul>
+  </div>
+
+
+
+</nav>
